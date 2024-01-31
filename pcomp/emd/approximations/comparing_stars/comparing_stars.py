d2ff3de99a3176c77159806d89d770e88c36d529
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-01-31 14:08:52 +0100

    Implement "Comparing Stars" GED Approximation
    Does not support edge labels (or additional attributes on nodes)
    
    But that will be looked into

---
+++ pcomp/emd/approximations/comparing_stars/comparing_stars.py
@@ -0,0 +1,286 @@
+from collections import Counter
+from dataclasses import dataclass
+from functools import cache
+from scipy.optimize import linear_sum_assignment
+from itertools import product
+import numpy as np
+
+EPSILON_NODE_LABEL = "pcomp:ged_approx:epsilon"
+
+
+@dataclass(frozen=True)
+class GraphNode:
+    label: str
+
+    # data: dict[str, Any] = field(default_factory=dict)
+    def __repr__(self) -> str:
+        return self.label
+
+
+@dataclass
+class DiGraph:
+    nodes: list[GraphNode]
+    edges: list[tuple[GraphNode, GraphNode]]
+
+    def degree(self, node: GraphNode) -> int:
+        return len([e for e in self.edges if e[0] == node])
+
+    def max_degree(self) -> int:
+        return max(self.degree(v) for v in self.nodes)
+
+    def node_ordering(self) -> list[GraphNode]:
+        return sorted(self.nodes, key=id)  # Sort nodes by their identity
+
+    def adjacency_matrix(self) -> np.ndarray:
+        ordering = self.node_ordering()
+        matrix = np.zeros((len(ordering), len(ordering)))
+
+        for u, v in self.edges:
+            matrix[ordering.index(u), ordering.index(v)] = 1
+
+        return matrix
+
+
+@dataclass(frozen=True)
+class Star:
+    root: GraphNode
+    leaves: frozenset[GraphNode]
+
+    def __repr__(self) -> str:
+        return f"({self.root}, {self.leaves})"
+
+
+def graph_edit_distance(graph_1: DiGraph, graph_2: DiGraph) -> tuple[float, float]:
+    """Compute an upper and lower bound on the Graph edit distance of the two graphs
+    This is based on the paper "Comparing Sttars: On Approximating Graph Edit Distance" by Zeng et al.
+    This algorithm is not defined for self-loops, multi-edges or edge labels. If self-loops exist, an error will be thrown
+
+    Args:
+        graph_1 (DiGraph): The first graph
+        graph_2 (DiGraph): The second graph
+
+    Returns:
+        tuple[float, float]: The computed bounds on the edit distance
+    """
+
+    if _has_self_loops(graph_1) or _has_self_loops(graph_2):
+        raise ValueError("This algorithm is not defined for graphs with self-loops")
+
+    # Make graphs have the same number of nodes
+    _normalize_graphs(graph_1, graph_2)
+
+    star_rep_1 = extract_star_representation(graph_1)
+    star_rep_2 = extract_star_representation(graph_2)
+
+    mapping_distance, permutation_matrix = graph_edit_distance_stars(
+        star_rep_1, star_rep_2, graph_1, graph_2
+    )
+    coefficient = max(4, max(graph_1.max_degree(), graph_2.max_degree()) + 1)
+    lower_bound = mapping_distance / coefficient
+
+    star_edit_distance.cache_clear()
+
+    upper_bound = refined_upper_bound(graph_1, graph_2, permutation_matrix)
+    return lower_bound, upper_bound
+
+
+def _has_self_loops(graph: DiGraph) -> bool:
+    """Check if the graph contains any self loops (loops of a node to itself)
+
+    Args:
+        graph (DiGraph): The graph
+
+    Returns:
+        bool: True if any self loops exist
+    """
+    return any(u == v for u, v in graph.edges)
+
+
+def _normalize_graphs(graph_1: DiGraph, graph_2: DiGraph) -> None:
+    """Normalize the graphs w.r.t. eachother. This is done by adding epsilon ("dummy") nodes to the smaller graph. All epsilon nodes have the same label
+
+    Args:
+        graph_1 (DiGraph): The first graph.
+        graph_2 (DiGraph): The second graph.
+    """
+    difference = len(graph_1.nodes) - len(graph_2.nodes)
+    if difference > 0:
+        graph_2.nodes.extend([GraphNode(EPSILON_NODE_LABEL)] * difference)
+    elif difference < 0:
+        graph_1.nodes.extend([GraphNode(EPSILON_NODE_LABEL)] * -difference)
+
+
+def extract_star_representation(graph: DiGraph) -> Counter[Star]:
+    """Extract the star representation for a graph. This is a multiset (counter) of stars.
+    This is done by extracting a star using each node in the graph as root once.
+
+    Args:
+        graph (DiGraph): The graph to extract the star representation from.
+
+    Returns:
+        Counter[Star]: A multiset of stars.
+    """
+    return Counter(
+        [
+            Star(root=r, leaves=frozenset(u for u, v in graph.edges if v == r))
+            for r in graph.nodes
+        ]
+    )
+
+
+@cache
+def star_edit_distance(star_1: Star, star_2: Star) -> float:
+    """Compute the edit distance between two stars.
+
+    Args:
+        star_1 (Star): The first star.
+        star_2 (Star): The second star.
+
+    Returns:
+        float: The edit distance between the two stars.
+    """
+    t = 0 if star_1.root.label == star_2.root.label else 1
+    labels_multiset_1 = Counter([l.label for l in star_1.leaves])
+    labels_multiset_2 = Counter([l.label for l in star_2.leaves])
+
+    m = (
+        max(labels_multiset_1.total(), labels_multiset_2.total())
+        - (labels_multiset_1 & labels_multiset_2).total()
+    )
+
+    d = abs(len(star_1.leaves) - len(star_2.leaves)) + m
+
+    return t + d
+
+
+def graph_edit_distance_stars(
+    stars_1: Counter[Star], stars_2: Counter[Star], graph_1: DiGraph, graph_2: DiGraph
+) -> tuple[float, np.ndarray]:
+    """Compute a lower bound on the graph edit distance by computing a min-weight full matching between the stars found in them.
+
+    Args:
+        star_1 (Star): The first star.
+        star_2 (Star): The second star.
+
+    Returns:
+        float: The edit distance between the two stars.
+    """
+    nodes_1 = list(stars_1.elements())
+    nodes_2 = list(stars_2.elements())
+
+    cost_matrix = np.empty((len(nodes_1), len(nodes_2)), dtype=float)
+    for u in nodes_1:
+        for v in nodes_2:
+            cost_matrix[nodes_1.index(u), nodes_2.index(v)] = star_edit_distance(u, v)
+
+    row_ind, col_ind = linear_sum_assignment(cost_matrix, maximize=False)
+    lower_bound = cost_matrix[row_ind, col_ind].sum()
+
+    # Map star indices to the index of their root
+    graph_1_index = graph_1.node_ordering()
+    graph_2_index = graph_2.node_ordering()
+
+    new_row_ind = np.array([graph_1_index.index(nodes_1[i].root) for i in row_ind])
+    new_col_ind = np.array([graph_2_index.index(nodes_2[i].root) for i in col_ind])
+
+    permutation_matrix = np.zeros((len(nodes_1), len(nodes_2)))
+    permutation_matrix[new_row_ind, new_col_ind] = 1
+
+    return lower_bound, permutation_matrix
+
+
+def calculate_transformation_cost(
+    graph_1: DiGraph, graph_2: DiGraph, permutation_matrix: np.ndarray
+) -> float:
+    """Calculate the cost of transforming graph_1 into graph_2 using the given permutation matrix for edit operations.
+
+    Equation 1 from the paper (Section 3.1)
+
+    Args:
+        graph_1 (DiGraph): The first graph.
+        graph_2 (DiGraph): The second graph.
+        permutation_matrix (np.ndarray): The permutation matrix. A 2D matrix containing a 1 for each pair of nodes that are mapped to eachother.
+
+    Returns:
+        float: The transformation cost.
+    """
+
+    ordering_1 = graph_1.node_ordering()
+    adj_1 = graph_1.adjacency_matrix()
+    ordering_2 = graph_2.node_ordering()
+    adj_2 = graph_2.adjacency_matrix()
+
+    if len(ordering_1) != len(ordering_2):
+        raise ValueError("The graphs must have the same number of nodes")
+
+    return sum(
+        (0 if ordering_1[i].label == ordering_1[j].label else 1)
+        * permutation_matrix[i, j]
+        for i in range(len(ordering_1))
+        for j in range(len(ordering_1))
+    ) + 0.5 * np.linalg.norm(
+        adj_1 - (permutation_matrix * adj_2 * permutation_matrix.T), ord=1
+    )
+
+    # res = sum(
+    #     sum(
+    #         (0 if ordering_1[i].label == ordering_1[j].label else 1)
+    #         * permutation_matrix[i, j]
+    #         + (
+    #             0.5
+    #             * (
+    #                 adj_1
+    #                 - np.linalg.norm(
+    #                     permutation_matrix * adj_2 * permutation_matrix.T, ord=1
+    #                 )
+    #             )
+    #         )
+    #         for j in range(len(ordering_1))
+    #     )
+    #     for i in range(len(ordering_1))
+    # )
+
+    # print(res)
+    # return res
+
+
+def refined_upper_bound(
+    graph_1: DiGraph, graph_2: DiGraph, permutation_matrix: np.ndarray
+) -> float:
+    """Compute an upper bound on the graph edit distance between the two graphs. Guaranteed to be terminated in 2n + n^2 steps.
+
+    Args:
+        graph_1 (DiGraph): The first graph.
+        graph_2 (DiGraph): The second graph.
+        permutation_matrix (np.ndarray): The permutation matrix yielded for the lower bound.
+
+    Returns:
+        float: The upper bound on the graph edit distance.
+    """
+    dist = calculate_transformation_cost(graph_1, graph_2, permutation_matrix)
+
+    min_dist = dist
+    min_permutation = permutation_matrix.copy()
+
+    graph_1_index = graph_1.node_ordering()
+
+    for u, v in product(graph_1.nodes, graph_1.nodes):
+        # Swap their matching
+        new_permutation = permutation_matrix.copy()
+        index_u = graph_1_index.index(u)
+        index_v = graph_1_index.index(v)
+
+        old_u = permutation_matrix[index_u, :]
+        new_permutation[index_u, :] = new_permutation[index_v, :]
+        new_permutation[index_v, :] = old_u
+
+        new_dist = calculate_transformation_cost(graph_1, graph_2, permutation_matrix)
+
+        if new_dist < min_dist:
+            min_dist = new_dist
+            min_permutation = new_permutation
+
+    if min_dist < dist:
+        return refined_upper_bound(graph_1, graph_2, min_permutation)
+
+    return min_dist
---
+++ tests/emd/approximations/comparing_stars/test_comparing_stars.py
@@ -0,0 +1,200 @@
+from collections import Counter
+import numpy as np
+from pytest import fixture
+from pcomp.emd.approximations.comparing_stars.comparing_stars import (
+    DiGraph,
+    GraphNode,
+    Star,
+    extract_star_representation,
+    _normalize_graphs,
+    graph_edit_distance_stars,
+    graph_edit_distance,
+)
+from networkx import graph_edit_distance as nx_graph_edit_distance
+from networkx import DiGraph as nx_DiGraph
+
+
+@fixture
+def graph_1() -> DiGraph:
+    a = GraphNode("a")
+    b = GraphNode("b")
+    c = GraphNode("c")
+    d = GraphNode("d")
+
+    # "Undirected"
+    edges = [(a, b), (b, a), (a, d), (d, a), (b, c), (c, b), (d, c), (c, d)]
+
+    return DiGraph(nodes=[a, b, c, d], edges=edges)
+
+
+@fixture
+def graph_2() -> DiGraph:
+    a = GraphNode("a")
+    b = GraphNode("b")
+    c = GraphNode("c")
+
+    edges = [(a, b), (b, a), (a, c), (c, a), (b, c), (c, b)]
+
+    return DiGraph(nodes=[a, b, c], edges=edges)
+
+
+@fixture
+def graph_fig_4a() -> DiGraph:
+    a = GraphNode("a")
+    b = GraphNode("b")
+    c = GraphNode("c")
+    d = GraphNode("d")
+    e = GraphNode("e")
+    f = GraphNode("f")
+
+    edges = [
+        (a, b),
+        (a, c),
+        (b, a),
+        (b, c),
+        (c, a),
+        (c, b),
+        (c, d),
+        (c, e),
+        (c, f),
+        (d, c),
+        (e, c),
+        (f, c),
+    ]
+
+    return DiGraph(nodes=[a, b, c, d, e, f], edges=edges)
+
+
+@fixture
+def graph_fig_4b() -> DiGraph:
+    a = GraphNode("a")
+    b = GraphNode("b")
+    c = GraphNode("c")
+    i = GraphNode("i")
+
+    edges = [
+        (a, b),
+        (a, c),
+        (a, i),
+        (b, a),
+        (b, c),
+        (b, i),
+        (c, a),
+        (c, b),
+        (c, i),
+        (i, a),
+        (i, b),
+        (i, c),
+    ]
+
+    return DiGraph(nodes=[a, b, c, i], edges=edges)
+
+
+@fixture
+def graph_fig_4c() -> DiGraph:
+    a = GraphNode("a")
+    b = GraphNode("b")
+    c = GraphNode("c")
+    n = GraphNode("n")
+
+    edges = [(a, b), (a, c), (b, a), (b, c), (c, a), (c, b), (c, n), (n, c)]
+
+    return DiGraph(nodes=[a, b, c, n], edges=edges)
+
+
+def test_star_extraction_graph_1(graph_1):
+    nodes = graph_1.nodes
+
+    node_a = nodes[0]
+    node_b = nodes[1]
+    node_c = nodes[2]
+    node_d = nodes[3]
+
+    stars = extract_star_representation(graph_1)
+
+    assert stars == Counter(
+        [
+            Star(node_a, frozenset([node_b, node_d])),
+            Star(node_b, frozenset([node_a, node_c])),
+            Star(node_c, frozenset([node_b, node_d])),
+            Star(node_d, frozenset([node_a, node_c])),
+        ]
+    )
+
+
+def test_star_extraction_graph_2(graph_2):
+    nodes = graph_2.nodes
+    node_a = nodes[0]
+    node_b = nodes[1]
+    node_c = nodes[2]
+
+    stars = extract_star_representation(graph_2)
+
+    assert stars == Counter(
+        [
+            Star(node_a, frozenset([node_b, node_c])),
+            Star(node_b, frozenset([node_a, node_c])),
+            Star(node_c, frozenset([node_a, node_b])),
+        ]
+    )
+
+
+def test_graph_edit_distance_stars_mapping_correct(graph_1, graph_2):
+    _normalize_graphs(graph_1, graph_2)
+    node_a_1 = graph_1.nodes[0]
+    node_b_1 = graph_1.nodes[1]
+    node_c_1 = graph_1.nodes[2]
+    node_d_1 = graph_1.nodes[3]
+
+    node_a_2 = graph_2.nodes[0]
+    node_b_2 = graph_2.nodes[1]
+    node_c_2 = graph_2.nodes[2]
+    node_epsilon_2 = graph_2.nodes[3]
+
+    # Extract stars
+    stars_1 = extract_star_representation(graph_1)
+    stars_2 = extract_star_representation(graph_2)
+
+    # Compute edit distance
+    mapping_distance, permutation_matrix = graph_edit_distance_stars(
+        stars_1, stars_2, graph_1, graph_2
+    )
+
+    # Check that the permutation matrix is correct
+
+    correct_permutation_matrix = np.zeros((len(graph_1.nodes), len(graph_2.nodes)))
+    graph_1_index = graph_1.node_ordering()
+    graph_2_index = graph_2.node_ordering()
+
+    correct_permutation_matrix[  # a -> a
+        graph_1_index.index(node_a_1), graph_2_index.index(node_a_2)
+    ] = 1
+    correct_permutation_matrix[  # b -> b
+        graph_1_index.index(node_b_1), graph_2_index.index(node_b_2)
+    ] = 1
+    correct_permutation_matrix[  # c -> c
+        graph_1_index.index(node_c_1), graph_2_index.index(node_c_2)
+    ] = 1
+    correct_permutation_matrix[  # d -> epsilon
+        graph_1_index.index(node_d_1), graph_2_index.index(node_epsilon_2)
+    ] = 1
+
+    assert np.array_equal(permutation_matrix, correct_permutation_matrix)
+
+
+def test_ged_bounds(graph_fig_4a, graph_fig_4b, graph_fig_4c):
+    # Create networkx graphs
+    nx_graph_a = nx_DiGraph([(u.label, v.label) for u, v in graph_fig_4a.edges])
+    nx_graph_b = nx_DiGraph([(u.label, v.label) for u, v in graph_fig_4b.edges])
+    nx_graph_c = nx_DiGraph([(u.label, v.label) for u, v in graph_fig_4c.edges])
+
+    # Graphs Fig 4a and Fig 4c - Real distance is 5
+    lower_bound_ac, upper_bound_ac = graph_edit_distance(graph_fig_4a, graph_fig_4c)
+    assert lower_bound_ac <= 5
+    assert 5 <= upper_bound_ac
+
+    # Graphs Fig 4b and Fig 4c - Real distance is 3
+    lower_bound_bc, upper_bound_bc = graph_edit_distance(graph_fig_4b, graph_fig_4c)
+
+    assert lower_bound_bc <= 3
+    assert 3 <= upper_bound_bc
