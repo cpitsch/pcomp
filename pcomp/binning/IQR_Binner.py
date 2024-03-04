75d26af04d69065b565fb466db736dc2d9d08a58
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-03-04 18:07:18 +0100

    Move OuterPercentileBinner into its own Module

--- pcomp/binning/IQR_Binner.py
+++ pcomp/binning/IQR_Binner.py
@@ -9,8 +9,8 @@ class IQR_Binner(Binner):
 
     def __init__(self, data: list[float], seed: int | None = None):
         super().__init__(data, seed)
-        self.Q_1 = np.percentile(self.data, 25)
-        self.Q_3 = np.percentile(self.data, 75)
+        self.Q_1 = np.percentile(self.data, 25).astype(float)
+        self.Q_3 = np.percentile(self.data, 75).astype(float)
 
     def bin(self, data: float) -> int:
         if data < self.Q_1:
@@ -22,37 +22,3 @@ class IQR_Binner(Binner):
         else:
             # "Value is higher than usual"
             return 2
-
-
-# IQR_Binner would be OuterPercentileBiner(data, 25)
-class OuterPercentileBinner(Binner):
-    lower_boundary: float
-    upper_boundary: float
-
-    def __init__(self, data: list[float], outer_percent: float = 10):
-        super().__init__(data)
-        self.lower_boundary = np.percentile(self.data, outer_percent)
-        self.upper_boundary = np.percentile(self.data, 100 - outer_percent)
-
-    def bin(self, data: float) -> int:
-        if data < self.lower_boundary:
-            # Value is lower than usual
-            return 0
-        elif data < self.upper_boundary:
-            # Value is a typical value
-            return 1
-        else:
-            # Value is higher than usual
-            return 2
-
-
-# IQR_Binner could be implemented as follows:
-# class IQRBinner2(Binner):
-#     inner_binner: OuterPercentileBinner
-
-#     def __init__(self, data: list[float]):
-#         super().__init__(data)
-#         self.inner_binner = OuterPercentileBinner(data, 25)
-
-#     def bin(self, data: float) -> int:
-#         return self.inner_binner.bin(data)
--- pcomp/binning/__init__.py
+++ pcomp/binning/__init__.py
@@ -1,3 +1,4 @@
-from .IQR_Binner import IQR_Binner, OuterPercentileBinner
+from .IQR_Binner import IQR_Binner
+from .OuterPercentileBinner import OuterPercentileBinner
 from .KMeans_Binner import KMeans_Binner
 from .Binner import Binner, BinnerManager
