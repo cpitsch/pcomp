11f013bf031e16e29cc16f4344bcca3c32582a21
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-03-04 20:44:38 +0100

    Move GED Approximate Comparator to own module

---
+++ pcomp/emd/Comparators/comparing_stars/ged_approximate_comparator.py
@@ -0,0 +1,104 @@
+from typing import Literal
+from pcomp.binning.Binner import BinnerManager
+from pcomp.binning.KMeans_Binner import KMeans_Binner
+from pcomp.emd.approximations.comparing_stars import DiGraph, GraphNode
+from pcomp.emd.approximations.comparing_stars.comparing_stars import (
+    timed_star_graph_edit_distance,
+)
+from pcomp.emd.core import BootstrappingStyle, EMD_ProcessComparator, EMDBackend
+import pandas as pd
+
+from pcomp.emd.emd import (
+    extract_service_time_traces,
+    extract_traces_activity_service_times,
+)
+
+
+class Timed_ApproxTraceGED_EMD_Comparator(EMD_ProcessComparator[DiGraph]):
+    """An implementation of the EMD_ProcessComparator for comparing event logs
+    w.r.t. the timed-control-flow, converting BinnedServiceTimeTraces to graphs
+    and applying the GED approximation from the paper "Comparing Stars: On
+    Approximating Graph Edit Distance" by Zeng et al.
+    """
+
+    binner_manager: BinnerManager
+
+    def __init__(
+        self,
+        log_1: pd.DataFrame,
+        log_2: pd.DataFrame,
+        bootstrapping_dist_size: int = 10000,
+        resample_size: int | float | None = None,
+        verbose: bool = True,
+        cleanup_on_del: bool = True,
+        bootstrapping_style: BootstrappingStyle = "replacement sublogs",
+        emd_backend: EMDBackend = "wasserstein",
+        num_bins: int = 3,
+        seed: int | None = None,
+    ):
+        super().__init__(
+            log_1,
+            log_2,
+            bootstrapping_dist_size,
+            resample_size,
+            verbose,
+            cleanup_on_del,
+            bootstrapping_style,
+            emd_backend,
+        )
+        self.num_bins = num_bins
+        self.seed = seed
+
+    def extract_representations(
+        self, log_1: pd.DataFrame, log_2: pd.DataFrame
+    ) -> tuple[list[DiGraph], list[DiGraph]]:
+        traces_1 = extract_service_time_traces(log_1)
+
+        self.binner_manager = BinnerManager(
+            [evt for trace in traces_1 for evt in trace],
+            KMeans_Binner,
+            k=self.num_bins,
+            seed=self.seed,
+        )
+
+        binned_traces_1 = extract_traces_activity_service_times(
+            log_1, self.binner_manager
+        )
+        binned_traces_2 = extract_traces_activity_service_times(
+            log_2, self.binner_manager
+        )
+
+        # Create the Graphs
+        graphs_1 = [
+            DiGraph(
+                nodes=tuple(GraphNode(label, duration) for label, duration in trace),
+                edges=frozenset((i, i + 1) for i in range(len(trace) - 1)),
+            )
+            for trace in binned_traces_1
+        ]
+        graphs_2 = [
+            DiGraph(
+                nodes=tuple(GraphNode(label, duration) for label, duration in trace),
+                edges=frozenset((i, i + 1) for i in range(len(trace) - 1)),
+            )
+            for trace in binned_traces_2
+        ]
+
+        return (graphs_1, graphs_2)
+
+    def cost_fn(
+        self,
+        item1: DiGraph,
+        item2: DiGraph,
+        bound: Literal["lower", "upper"] = "lower",
+    ) -> float:
+        # Scale time differences by the largest possible time difference, num_bins - 1
+        lower_bound, upper_bound = timed_star_graph_edit_distance(
+            item1, item2, self.num_bins - 1
+        )
+
+        # For now, for testing, we only use the lower bound because `compare` doesn't pass the bound in.
+        return lower_bound if bound == "lower" else upper_bound
+
+    def cleanup(self) -> None:
+        timed_star_graph_edit_distance.cache_clear()
