c17cae2c15f87e57098793561c26c34fe3387d6d
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-03-20 12:33:10 +0100

    OuterpercentileBinner: Add (unused) seed to be compliant with Binner abstract class

--- pcomp/binning/OuterPercentileBinner.py
+++ pcomp/binning/OuterPercentileBinner.py
@@ -8,7 +8,19 @@ class OuterPercentileBinner(Binner):
     upper_boundary: float
     num_bins: int = 3
 
-    def __init__(self, data: list[float], outer_percent: float = 10):
+    def __init__(
+        self, data: list[float], outer_percent: float = 10, seed: int | None = None
+    ):
+        """Create an OuterPercentileBinner which bins data into three bins:
+          1. The lower `outer_percent`% of values
+          2. The middle
+          3. The upper `outer_percent`% of values
+
+        Args:
+            data (list[float]): The data to bin
+            outer_percent (float, optional): The percent to bin in a separate bin at each end of the distribution. Defaults to 10.
+            seed (int | None, optional): The seed used for binning. Not used in OuterPercentileBinner. Defaults to None.
+        """
         super().__init__(data)
         self.lower_boundary = np.percentile(self.data, outer_percent).astype(float)
         self.upper_boundary = np.percentile(self.data, 100 - outer_percent).astype(
