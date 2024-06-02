62d4b0c74ee576af9c52d38279ecfbcb9f79c95a
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-06-02 17:34:26 +0200

    Move first approach files into first approach module
    First approach not needed at all anymore. Will remove entirely at a
    later point

--- pcomp/first_approach/algorithm.py
+++ pcomp/first_approach/algorithm.py
@@ -6,8 +6,8 @@ import numpy as np
 import pandas as pd
 from scipy.stats import chi2_contingency  # type: ignore
 
+from pcomp.first_approach.data import Binner, create_binner
 from pcomp.utils import constants, ensure_start_timestamp_column
-from pcomp.utils.data import Binner, create_binner
 from pcomp.utils.typing import Numpy1DArray
 
 T = TypeVar("T")
